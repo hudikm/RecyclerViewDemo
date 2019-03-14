e9fdf25ed01ff420520f4912e50b9a5000b66c93
Martin Hudik <hudikm@gmail.com>
2019-03-14 20:31:08 +0100

    3.0 Fragmenty

--- app/src/main/java/sk/fri/uniza/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/sk/fri/uniza/recyclerviewdemo/MainActivity.java
@@ -2,41 +2,33 @@ package sk.fri.uniza.recyclerviewdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
-
-import sk.fri.uniza.recyclerviewdemo.model.RecipesDataSet;
-
 
 public class MainActivity extends AppCompatActivity implements MyRecipeRecyclerViewAdapter.OnRecipeListInteraction {
-
-
-    @Override
-    public void onRecipeClick(int position) {
-        Toast.makeText(this, RecipesDataSet.getRecipes(this).get(position).title, Toast.LENGTH_LONG).show();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recycler_list_layout);
+        setContentView(R.layout.main_layout);
 
-        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        // Testovanie či už fragmenty neexistujú
+        if ((getSupportFragmentManager().findFragmentByTag("detailFragment") == null)
+                && (getSupportFragmentManager().findFragmentByTag("listFragment") == null)) {
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new ListFragment(), "listFragment")
+                    .commit();
+        }
 
 
-        // specify an adapter (see also next example)
-        MyRecipeRecyclerViewAdapter mAdapter = new MyRecipeRecyclerViewAdapter(RecipesDataSet.getRecipes(this), this);
-        recyclerView.setAdapter(mAdapter);
+    }
 
+    // Vytvorenie detail fragmentu
+    @Override
+    public void onRecipeClick(int position) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, DetailFragment.createDetailFragment(position), "detailFragment")
+                .addToBackStack(null)
+                .commit();
     }
 
 }
